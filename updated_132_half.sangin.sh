716ae867ef49aff2a852c4074914e65f5d48452a
NIKA1371 <alex.jafari5555@gmail.com>
2025-07-31 13:39:30 +0330

    Update updated_132_half.sangin.sh

--- updated_132_half.sangin.sh
+++ updated_132_half.sangin.sh
@@ -147,7 +147,10 @@ fi
 log "Creating poststart.sh..."
 cat > poststart.sh <<EOF
 #!/bin/bash
-for i in {1..10}; do ip link show wtun0 && break; sleep 1; done
+# تلاش برای ایجاد رابط TUN (wtun0)
+ip tuntap add dev wtun0 mode tun
+ip link set dev wtun0 up
+# تنظیم MTU برای eth0 و wtun0
 ip link set dev eth0 mtu 1420 || true
 ip link set dev wtun0 mtu 1420 || true
 EOF
