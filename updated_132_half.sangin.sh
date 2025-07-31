fb7c2cc69c19fe726a25f199de0feec3a785d7d5
NIKA1371 <alex.jafari5555@gmail.com>
2025-07-31 15:43:41 +0330

    Update updated_132_half.sangin.sh

--- updated_132_half.sangin.sh
+++ updated_132_half.sangin.sh
@@ -3,8 +3,8 @@ set -e
 
 INSTALL_DIR="/root/packettunnel"
 SERVICE_FILE="/etc/systemd/system/packettunnel.service"
-CORE_URL="https://raw.githubusercontent.com/mahdipatriot/PacketTunnel/main/core.json"
-WATERWALL_URL="https://raw.githubusercontent.com/mahdipatriot/PacketTunnel/main/Waterwall"
+CORE_URL="https://raw.githubusercontent.com/NIKA1371/test/main/core.json"
+WATERWALL_URL="https://raw.githubusercontent.com/NIKA1371/test/main/Waterwall"
 
 log() {
     echo -e "[+] $1"
